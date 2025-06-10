bc5b813ee3ef7310d8af30c40c22cfdd6c01e86f
mateus00000 <mateus.c000@outlook.com>
2025-06-09 21:25:51 -0300

    Deixando o IP estatico

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import constants from 'expo-constants';
 
-const API_URL =
-  constants.expoConfig?.extra?.API_URL || 
-  constants.manifest?.extra?.API_URL || 
-    
-  'http://localhost:3000';         
+const API_URL = 'http://localhost:3000';         
+  
+  console.log(API_URL)
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
